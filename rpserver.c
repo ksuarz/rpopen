d3bc92969afcaea3dc84396f311ae6ea856dc5c4
Kyle Suarez <ksuarz@gmail.com>
2014-10-01 23:10:02 -0400

    Begin implementation of rpserver
    Add getopt code to parse command-line arguments.

--- rpserver.c
+++ rpserver.c
@@ -1,7 +1,35 @@
 /*
-	rpserver: remote popen server
-*/
+ * rpserver.c - remote rpopen server
+ * Kyle Suarez
+ */
 
-main(int argc, char **argv)
-{
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * Runs an rpserver for use with rpopen.
+ */
+int
+main (int argc, char **argv) {
+    int port;
+    int c;
+
+    /* Parse command line arguments */
+    opterr = 1;
+    while (opt = getopt(argc, argv, "hp:") != -1) {
+        switch (opt) {
+            case 'p':
+                port = (int) (strtol(optarg, NULL, 0));
+                break;
+            case 'h':
+                printf("Usage: rpserver [-p PORT_NUM]\n");
+                return EXIT_SUCCESS;
+            default:
+                fprintf(stderr, "Unknown option: %c\n", opt);
+                return EXIT_FAILURE;
+        }
+    }
+
+    /* TODO */
 }
