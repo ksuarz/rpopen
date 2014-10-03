82dee1b876df85abbd6ccfd88920f9349a372e5d
Kyle Suarez <ksuarz@gmail.com>
2014-10-02 23:13:20 -0400

    Finish first draft of rpserver
    Full outline of functionality required from the server. The read(2) call in the
    child after the fork needs to be checked if it requires multiple reads if
    reading from a socket.
    
    A future commit will modularize the code.

--- rpserver.c
+++ rpserver.c
@@ -15,20 +15,36 @@
 #define BACKLOG 3
 
 /**
+ * Default port number.
+ */
+#define DEFAULT_PORT 0
+
+/**
+ * Max size for input command buffer. (That is, commands cannot exceed this
+ * length in bytes.)
+ */
+#define MAXBUFSIZE 2048
+
+/**
  * Runs an rpserver for use with rpopen.
  */
 int
 main (int argc, char **argv) {
+    char buffer[MAXBUFSIZE];
     int opt;
     int port;
-    int sock;
+    int request_sock;
+    int server_sock;
     int sockoptval;
+    pid_t pid;
+    size_t socklen;
     socklen_t addr_len;
+    ssize_t bytes_read;
     struct sockaddr_in client_addr;
     struct sockaddr_in server_addr;
 
     opterr = 1;
-    port = 0;
+    port = DEFAULT_PORT;
 
     /* Parse command line arguments */
     while (opt = getopt(argc, argv, "hp:") != -1) {
@@ -50,27 +66,76 @@ main (int argc, char **argv) {
     }
 
     /* Create a TCP socket */
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    if ((server_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("Failed to create a socket");
         return EXIT_FAILURE;
     }
-    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &sockoptval, sizeof(int));
+    setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &sockoptval, sizeof(int));
 
     /* Bind socket */
-    memset(&server_addr, 0, sizeof(struct sockaddr_in));
+    socklen = sizeof(struct sockaddr_in);
+    memset(&server_addr, 0, socklen);
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port);
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    if (bind(sock, (struct sockaddr *) &server_addr, sizeof(struct sockaddr_in)) < 1) {
+    if (bind(server_sock, (struct sockaddr *) &server_addr, socklen) < 1) {
         perror("Bind failure");
         return EXIT_FAILURE;
     }
 
     /* Listen on the socket */
-    if (listen(sock, BACKLOG) < 0) {
+    if (listen(server_sock, BACKLOG) < 0) {
         perror("Failed to listen on socket");
         return EXIT_FAILURE;
     }
-    
-    /* TODO */
+
+    /* Accept connections */
+    while (1) {
+        while ((request_sock = accept(server_sock, (struct sockaddr *) &client_addr, socklen)) < 0) {
+            /* Error handling */
+            switch(errno) {
+                case ECHILD:
+                case ERESTART:
+                case EINTR:
+                    /* Ignore and restart */
+                    break;
+                default:
+                    perror("Accept faulure");
+                    return EXIT_FAILURE;
+            }
+        }
+
+        /* Connection has been accepted */
+        pid = fork();
+        if (f == -1) {
+            /* Error */
+            perror("Fork failure");
+            return EXIT_FAILURE;
+        }
+        else if (f == 0) {
+            /* Child */
+            bytes_read = read(request_sock, buffer, MAXBUFSIZE);
+            if (bytes_read == -1) {
+                perror("Failure on socket read");
+                return EXIT_FAILURE;
+            }
+            /* TODO What if we need multiple reads? */
+
+            /* Duplicate our STDOUT onto the socket */
+            close(STDIN_FILENO);
+            close(STDOUT_FILENO);
+            dup2(request_sock, STDOUT_FILENO);
+
+            /* Execute the command */
+            execlp("/bin/sh", "/bin/sh -c", &buffer, (char *) 0);
+            perror("execlp(2) failure");
+            return EXIT_FAILURE;
+        }
+        else {
+            /* Parent */
+            shutdown(request_sock, SHUT_RD);
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
