c4777339f01d957c5c1c2d2518b97736d5b38275
Kyle Suarez <ksuarz@gmail.com>
2014-10-10 00:39:49 -0400

    Update documentation in rpopen.c and rpopen.h
    Formatting fixes and small nitpicks.

--- rpopen.c
+++ rpopen.c
@@ -13,7 +13,7 @@
 #include <unistd.h>
 
 /**
- * Creates an IPv4 socket.
+ * Creates a socket for connecting to the rpserver.
  *
  * @return A socket descriptor created by socket(2). If an error occurs, NULL is
  * returned and errno is set.
@@ -25,7 +25,7 @@ rpopen_get_socket (void) {
     /* Allocate space for the socket */
     sock = (int *) malloc(sizeof(int));
     if (!sock) {
-        perror("Failed to malloc(2) space for a socket");
+        perror("Failed to malloc(3) space for a socket");
         return NULL;
     }
 
@@ -40,9 +40,11 @@ rpopen_get_socket (void) {
 }
 
 /**
- * Binds an IPv4 socket for connecting to an rpserver.
+ * Binds a socket for connecting to an rpserver.
  *
- * @param sock A pointer to the socket to bind.
+ * @param sock A pointer to a socket obtained from rpopen_get_socket().
+ *
+ * @return 0 if the socket was bound properly; -1 otherwise.
  */
 int
 rpopen_bind_socket (int *sock) {
@@ -65,13 +67,13 @@ rpopen_bind_socket (int *sock) {
 /**
  * Implements process I/O on a remote host in a way similar to popen(2).
  *
- * @param host The name of the host to connect to. If NULL or empty, then it
+ * @param hostname The name of the host to connect to. If NULL or empty, then it
  * defaults to the PHOST environment variable.
  * @param port The port number to connect to on the remote host. If 0 is
  * specified, then this defaults to PPORT.
  * @param command A string containing the command to execute.
  *
- * @return An I/O stream that must be closed with pclose(2). Data can be read
+ * @return A socket stream that must be closed with pclose(2). Data can be read
  * from it using read(2). If an error occurs, the return value is NULL.
  */
 FILE *
--- rpopen.h
+++ rpopen.h
@@ -7,20 +7,20 @@
 /**
  * Implements process I/O on a remote host in a way similar to popen(2).
  *
- * @param host The name of the host to connect to. If NULL or empty, then it
+ * @param hostname The name of the host to connect to. If NULL or empty, then it
  * defaults to the PHOST environment variable.
  * @param port The port number to connect to on the remote host. If 0 is
  * specified, then this defaults to PPORT.
- * @param cmd A string containing the command to execute.
+ * @param command A string containing the command to execute.
  *
- * @return An I/O stream that must be closed with pclose(2). If an error occurs,
- * the return value is NULL.
+ * @return A socket stream that must be closed with pclose(2). Data can be read
+ * from it using read(2). If an error occurs, the return value is NULL.
  */
 FILE *
 rpopen (const char *, int, const char *);
 
 /**
- * Creates an IPv4 socket.
+ * Creates a socket for connecting to the rpserver.
  *
  * @return A socket descriptor created by socket(2). If an error occurs, NULL is
  * returned and errno is set.
@@ -29,9 +29,11 @@ int *
 rpopen_get_socket (void);
 
 /**
- * Binds an IPv4 socket for connecting to an rpserver.
+ * Binds a socket for connecting to an rpserver.
  *
- * @param sock A pointer to the socket to bind.
+ * @param sock A pointer to a socket obtained from rpopen_get_socket().
+ *
+ * @return 0 if the socket was bound properly; -1 otherwise.
  */
 int
 rpopen_bind_socket (int *);
