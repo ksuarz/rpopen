3bcb9ca2f6d6d4d5336a4b9bc8a03b78741aca90
Kyle Suarez <ksuarz@gmail.com>
2014-10-10 00:43:17 -0400

    Saving work

--- rpserver.c
+++ rpserver.c
@@ -3,27 +3,107 @@
  * Kyle Suarez
  */
 
+#include <netdb.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 
+/**
+ * Create a socket for listening for the rpopen server.
+ *
+ * @return A pointer to a descriptor for the socket. If an error occurs, NULL is
+ * returned.
+ */
+int
+rpserver_get_socket (void) {
+    int *sock;
+    int sockoptval;
+
+    /* Allocate space */
+    sock = (int *) malloc(sizeof(int));
+    if (!sock) {
+        perror("Failed to malloc(3) space for a socket");
+        return NULL;
+    }
+
+    /* Initialize socket */
+    *sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (*sock < 0) {
+        perror("Failed to create a socket");
+        return NULL;
+    }
+
+    /* Set socket options */
+    sockoptval = 1;
+    setsockopt(*sock, SOL_SOCKET, SO_REUSEADDR, &sockoptval, sizeof(int));
+    return sock;
+}
+
 /*
- * Used for listen(2) - the number of backlogged processes the socket will allow
- * before refusing new connections.
+ * Binds a socket ready for listening by the rpopen server.
+ *
+ * @param sock A pointer to the socket to bind.
+ * @param port The port for the socket to be bound to.
+ * @return 0 if the socket was bound properly; -1 otherwise.
  */
-#define BACKLOG 3
+int
+rpserver_bind_socket (int *sock, int port) {
+    struct sockaddr_in svr_addr;
+
+    memset(&svr_addr, 0, sizeof(struct sockaddr_in));
+    svr_addr.sin_family = AF_INET;
+    svr_addr.sin_port = htons(port);
+    svr_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(*sock, (struct sockaddr *) &svr_addr, sizeof(struct sockaddr_in)) < 1) {
+        perror("Bind failure");
+        return -1;
+    }
+    else {
+        return 0;
+    }
+}
 
 /**
- * Default port number.
+ * Executes a command read from the socket.
+ *
+ * @param socket sock socket descriptor from an accepted connection.
+ * @return On success, this performs an exec(2) and does not return. If an error
+ * occurs, errno is set.
  */
-#define DEFAULT_PORT 0
+void
+rpserver_exec_command (int socket) {
+    ssize_t bytes_read;
+    char cmd_buffer[MAXBUFSIZE];
+    ssize_t buflen;
 
-/**
- * Max size for input command buffer. (That is, commands cannot exceed this
- * length in bytes.)
- */
-#define MAXBUFSIZE 2048
+    /* Zero-out the buffer */
+    buflen = 0;
+    memset(&cmd_buffer, 0, MAXBUFSIZE);
+
+    /* Read the command from the client */
+    /* TODO refactor */
+    for (buflen = 0, bytes_read = 1; buflen < MAXBUFSIZE && bytes_read > 0; buflen += bytes_read) {
+        bytes_read = read(sock, &cmd_buffer + buflen, MAXBUFSIZE - buflen);
+    }
+    if (bytes_read == -1) {
+        perror("Failure on read(2) on socket");
+        return;
+    }
+
+    /* Duplicate our STDOUT onto the socket */
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    dup2(request_sock, STDOUT_FILENO);
+
+    /* Execute the command */
+    execlp("/bin/sh", "/bin/sh -c", &buffer, (char *) 0);
+    perror("execlp(2) failure");
+}
 
 /**
  * Runs an rpserver for use with rpopen.
@@ -35,11 +115,7 @@ main (int argc, char **argv) {
     int port;
     int request_sock;
     int server_sock;
-    int sockoptval;
     pid_t pid;
-    size_t socklen;
-    socklen_t addr_len;
-    ssize_t bytes_read;
     struct sockaddr_in client_addr;
     struct sockaddr_in server_addr;
 
@@ -54,8 +130,8 @@ main (int argc, char **argv) {
                 return EXIT_SUCCESS;
             case 'p':
                 port = (int) (strtol(optarg, NULL, 0));
-                if (errno != 0) {
-                    perror(NULL);
+                if (port == 0) {
+                    fprintf(stderr, "Invalid port: %s\n", optarg);
                     return EXIT_FAILURE;
                 }
                 break;
@@ -66,20 +142,13 @@ main (int argc, char **argv) {
     }
 
     /* Create a TCP socket */
-    if ((server_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Failed to create a socket");
+    server_sock = rpserver_get_socket();
+    if (server_sock == -1) {
         return EXIT_FAILURE;
     }
-    setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &sockoptval, sizeof(int));
 
     /* Bind socket */
-    socklen = sizeof(struct sockaddr_in);
-    memset(&server_addr, 0, socklen);
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    if (bind(server_sock, (struct sockaddr *) &server_addr, socklen) < 1) {
-        perror("Bind failure");
+    if (rpserver_bind_socket(server_sock) == -1) {
         return EXIT_FAILURE;
     }
 
@@ -98,37 +167,26 @@ main (int argc, char **argv) {
                 case ERESTART:
                 case EINTR:
                     /* Ignore and restart */
+                    fprintf("rpserver: Warning: Ignoring error %d\n", errno);
                     break;
                 default:
-                    perror("Accept faulure");
+                    perror("Accept failure");
                     return EXIT_FAILURE;
             }
         }
 
         /* Connection has been accepted */
         pid = fork();
-        if (f == -1) {
+        if (pid == -1) {
             /* Error */
             perror("Fork failure");
             return EXIT_FAILURE;
         }
-        else if (f == 0) {
+        else if (pid == 0) {
             /* Child */
-            bytes_read = read(request_sock, buffer, MAXBUFSIZE);
-            if (bytes_read == -1) {
-                perror("Failure on socket read");
-                return EXIT_FAILURE;
-            }
-            /* TODO What if we need multiple reads? */
+            rpserver_exec_command(request_sock);
 
-            /* Duplicate our STDOUT onto the socket */
-            close(STDIN_FILENO);
-            close(STDOUT_FILENO);
-            dup2(request_sock, STDOUT_FILENO);
-
-            /* Execute the command */
-            execlp("/bin/sh", "/bin/sh -c", &buffer, (char *) 0);
-            perror("execlp(2) failure");
+            /* If this is reached, exec(2) failed */
             return EXIT_FAILURE;
         }
         else {
@@ -137,5 +195,6 @@ main (int argc, char **argv) {
         }
     }
 
+    printf("rpserver exiting: goodbye\n");
     return EXIT_SUCCESS;
 }
